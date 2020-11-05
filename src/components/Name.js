91277319906414b340d3ff3612f1bf80ff196eb0
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-05 22:15:11 +0530

    Update Name.js
    modified style
    modified content

--- src/components/Name.js
+++ src/components/Name.js
@@ -4,19 +4,21 @@ const Name = () => {
   return (
     <div className="Name">
       <div
-        style={{ fontSize: 28, backgroundColor: "black", color: "cyan" }}
+        style={{ fontSize: 24, backgroundColor: "black", color: "cyan" }}
         className="container mt-3 p-5"
+        id="name-typed"
       >
         <Typed
           strings={[
-            "Hello.",
+            "<span style='color:#00ff80;'>Hello.</span>",
             "I am Rishabh Pathak.",
             "I am also known as Clumsyknight.",
-            "View my projects below.",
+            "A FullStack developer",
+            "<span style='color:#00ffbf;'>View my projects below.</span>",
           ]}
           typeSpeed={40}
           smartBackspace={true}
-          backSpeed={50}
+          backSpeed={60}
         />
       </div>
     </div>
