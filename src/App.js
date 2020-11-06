3ab3b4e22c1866180ebec1293fcbe77a25a3d7ab
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-06 10:48:37 +0530

    Added Navbar with elements for pageWrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar";
 import Name from "./components/Name";
+import { pushRotate as Menu } from "react-burger-menu";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="App" id="outer-container">
       <Navbar />
-      <Name />
+      <div id="page-wrap">
+        <Name />
+      </div>
     </div>
   );
 };
