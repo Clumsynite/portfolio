828a5232bd24ddf42f068295792e5f36e7f8eb6a
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-06 13:40:37 +0530

    changed menu animation, imported links stylesheet

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
-import { pushRotate as Menu } from "react-burger-menu";
+import { push as Menu } from "react-burger-menu";
 import "../styles/Navbar.css";
+import "../styles/Link.css";
 
 const Navbar = () => {
   return (
     <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"}>
-      <a id="home" className="menu-item">
+      <a id="home" className="menu-item link" href="/">
         Home
       </a>
     </Menu>
