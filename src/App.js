5a78487ef10b6177eb2ecdc97a05ffeb44f76e79
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-06 17:36:58 +0530

    added padding-top

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { pushRotate as Menu } from "react-burger-menu";
 
 const App = () => {
   return (
-    <div className="App" id="outer-container">
+    <div
+      className="App"
+      id="outer-container"
+      style={{ backgroundColor: "black", minHeight: "100vh" }}
+    >
       <Navbar />
-      <div id="page-wrap">
+      <div id="page-wrap" className="pt-4">
         <Name />
       </div>
     </div>
