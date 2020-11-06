c528770955ed7672546e803854cfe6abaf476c37
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-06 17:55:29 +0530

    Added new links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { push as Menu } from "react-burger-menu";
+import FitText from "@kennethormandy/react-fittext";
 import "../styles/Navbar.css";
 import "../styles/Link.css";
 
@@ -7,6 +8,12 @@ const Navbar = () => {
     <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"}>
       <a id="home" className="menu-item link" href="/">
         Home
+      </a>
+      <a id="projects" className="menu-item link" href="/">
+        Projects
+      </a>
+      <a id="contact" className="menu-item link" href="/">
+        Contact
       </a>
     </Menu>
   );
