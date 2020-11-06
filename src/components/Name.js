f39c2b4196a392c9c27319b4cb52a8f3fafda3bd
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-06 17:37:23 +0530

    modifed to use stylesheet instead of inline styles

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,25 +1,24 @@
 import Typed from "react-typed";
+import FitText from "@kennethormandy/react-fittext";
+import "../styles/Name.css";
 
 const Name = () => {
   return (
     <div className="Name">
-      <div
-        style={{ fontSize: 24, backgroundColor: "black", color: "cyan" }}
-        className="container mt-3 p-5"
-        id="name-typed"
-      >
-        <Typed
-          strings={[
-            "<span style='color:#00ff80;'>Hello.</span>",
-            "I am Rishabh Pathak.",
-            "I am also known as Clumsyknight.",
-            "A FullStack developer",
-            "<span style='color:#00ffbf;'>View my projects below.</span>",
-          ]}
-          typeSpeed={40}
-          smartBackspace={true}
-          backSpeed={60}
-        />
+      <div className="p-5 typed col-md-6" id="name-typed">
+        <FitText compressor={0.9}>
+          <Typed
+            strings={[
+              "<span style='color:#00ff80;'>Hello.</span>",
+              "I am Rishabh Pathak <span class='badge badge-pill badge-info'>AKA</span> Clumsyknight.",
+              "I am a FullStack developer.",
+              "<span style='color:#00ffbf;'>View my projects below.</span>",
+            ]}
+            typeSpeed={40}
+            smartBackspace={true}
+            backSpeed={60}
+          />
+        </FitText>
       </div>
     </div>
   );
