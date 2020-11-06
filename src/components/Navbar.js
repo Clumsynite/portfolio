d7cf80d868d4d1a49a688e239b8720e96d56e3ee
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-06 18:43:58 +0530

    added icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,29 @@
 import { push as Menu } from "react-burger-menu";
-import FitText from "@kennethormandy/react-fittext";
+import { InlineIcon } from "@iconify/react";
+import outlineHome from "@iconify/icons-ic/outline-home";
+import sharpContactPage from '@iconify/icons-ic/sharp-contact-page';
+import appSwitcher from "@iconify/icons-carbon/app-switcher";
 import "../styles/Navbar.css";
 import "../styles/Link.css";
 
 const Navbar = () => {
   return (
-    <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"}>
-      <a id="home" className="menu-item link" href="/">
-        Home
-      </a>
-      <a id="projects" className="menu-item link" href="/">
-        Projects
-      </a>
-      <a id="contact" className="menu-item link" href="/">
-        Contact
-      </a>
+    <Menu
+      pageWrapId={"page-wrap"}
+      outerContainerId={"outer-container"}
+      width={250}
+    >
+      <div className="d-flex flex-column justify-content-around h-50">
+        <a id="home" className="menu-item link" href="/">
+          <InlineIcon icon={outlineHome} /> Home
+        </a>
+        <a id="projects" className="menu-item link" href="/">
+          <InlineIcon icon={appSwitcher} /> Projects
+        </a>
+        <a id="contact" className="menu-item link" href="/">
+        <InlineIcon icon={sharpContactPage} /> Contact
+        </a>
+      </div>
     </Menu>
   );
 };
