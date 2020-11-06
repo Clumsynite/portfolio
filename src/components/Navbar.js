d653fdf533cd8e20528f1d933697c46fef88c2d4
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-06 19:20:59 +0530

    changed font size

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import { push as Menu } from "react-burger-menu";
 import { InlineIcon } from "@iconify/react";
 import outlineHome from "@iconify/icons-ic/outline-home";
-import sharpContactPage from '@iconify/icons-ic/sharp-contact-page';
+import sharpContactPage from "@iconify/icons-ic/sharp-contact-page";
 import appSwitcher from "@iconify/icons-carbon/app-switcher";
 import "../styles/Navbar.css";
 import "../styles/Link.css";
@@ -14,14 +14,14 @@ const Navbar = () => {
       width={250}
     >
       <div className="d-flex flex-column justify-content-around h-50">
-        <a id="home" className="menu-item link" href="/">
+        <a id="home" className="menu-item link font-em" href="/">
           <InlineIcon icon={outlineHome} /> Home
         </a>
-        <a id="projects" className="menu-item link" href="/">
+        <a id="projects" className="menu-item link  font-em" href="/">
           <InlineIcon icon={appSwitcher} /> Projects
         </a>
-        <a id="contact" className="menu-item link" href="/">
-        <InlineIcon icon={sharpContactPage} /> Contact
+        <a id="contact" className="menu-item link  font-em" href="/">
+          <InlineIcon icon={sharpContactPage} /> Contact
         </a>
       </div>
     </Menu>
