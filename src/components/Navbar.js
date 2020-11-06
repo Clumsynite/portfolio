b6e35c0c5127b96914bc73aa0967dde78acad25d
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-06 10:48:50 +0530

    modified to use react-hamburger-menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,14 @@
-import { Turn as Hamburger } from "hamburger-react";
+import { pushRotate as Menu } from "react-burger-menu";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"}>
+      <a id="home" className="menu-item">
+        Home
+      </a>
+    </Menu>
+  );
 };
 
 export default Navbar;
