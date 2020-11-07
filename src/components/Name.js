f1c5ba899a0885de6ea648e6a0c3dd508d6e4db3
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-07 16:32:08 +0530

    added classes to render text in center

--- src/components/Name.js
+++ src/components/Name.js
@@ -4,22 +4,20 @@ import "../styles/Name.css";
 
 const Name = () => {
   return (
-    <div className="Name">
-      <div className="p-5 typed col-md-6" id="name-typed">
-        <FitText compressor={1}>
-          <Typed
-            strings={[
-              "<span style='color:#EA0F1E;font-weight:bold'>Hello.</span>",
-              "I am Rishabh Pathak <span class='badge badge-pill badge-info'>AKA</span> Clumsyknight.",
-              "I am a FullStack developer.",
-              "<span style='color:#00ffbf;'>View my projects below.</span>",
-            ]}
-            typeSpeed={60}
-            smartBackspace={true}
-            backSpeed={50}
-          />
-        </FitText>
-      </div>
+    <div className="pl-4 typed col-md-6 align-self-center" id="name-typed">
+      <FitText compressor={1}>
+        <Typed
+          strings={[
+            "<span style='color:#EA0F1E;font-weight:bold'>Hello.</span>",
+            "I am Rishabh Pathak <span class='badge badge-pill badge-info'>AKA</span> Clumsyknight.",
+            "I am a FullStack developer.",
+            "<span style='color:#00ffbf;'>View my projects below.</span>",
+          ]}
+          typeSpeed={60}
+          smartBackspace={true}
+          backSpeed={50}
+        />
+      </FitText>
     </div>
   );
 };
