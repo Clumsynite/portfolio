4026400330ceeb58aad7d5728efc3276961ccc1a
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-07 18:46:39 +0530

    created array to render projects

---
+++ src/templates/projects.js
@@ -0,0 +1,11 @@
+const projects = [
+  {
+    title: "Blog-Read",
+    desc:
+      "Frontend built to read blog posts.\n Uses Blog API for authentication and database management.",
+    repo: "https://github.com/Clumsynite/blog-read",
+    live: "https://clumsynite.github.io/blog-read/",
+  },
+];
+
+export default projects;
