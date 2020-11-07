59b36c3656c035864d392011725b9076a769692c
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-07 19:45:21 +0530

    imported images in array

--- src/templates/projects.js
+++ src/templates/projects.js
@@ -1,3 +1,6 @@
+import BlogReadPNG from "../images/blog-read.png";
+import BlogReadGIF from "../images/blog-read.gif";
+
 const projects = [
   {
     title: "Blog-Read",
@@ -5,6 +8,8 @@ const projects = [
       "Frontend built to read blog posts.\n Uses Blog API for authentication and database management.",
     repo: "https://github.com/Clumsynite/blog-read",
     live: "https://clumsynite.github.io/blog-read/",
+    png: BlogReadPNG,
+    gif: BlogReadGIF
   },
 ];
 
