ac55ef2d1e0ff54b6d269d41d33fbc2c5921c730
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-07 22:15:24 +0530

    Added inventory screenshots

--- src/templates/projects.js
+++ src/templates/projects.js
@@ -2,6 +2,8 @@ import BlogReadPNG from "../images/blog-read.png";
 import BlogReadGIF from "../images/blog-read.gif";
 import BlogWritePNG from "../images/blog-write.png";
 import BlogWriteGIF from "../images/blog-write.gif";
+import InventoryAppPNG from "../images/inventory-app.png";
+import InventoryAppGIF from "../images/inventory-app.gif";
 
 const projects = [
   {
@@ -29,9 +31,16 @@ const projects = [
     desc: "Crud App for inventory Management",
     repo: "https://github.com/Clumsynite/inventory-app",
     live: "https://clumsy-inventory.herokuapp.com/item",
-    png: BlogWritePNG,
-    gif: BlogWriteGIF,
-    languages: { html: true, js: true, bootstrap: true, node: true, pug: true },
+    png: InventoryAppPNG,
+    gif: InventoryAppGIF,
+    languages: {
+      html: true,
+      js: true,
+      bootstrap: true,
+      node: true,
+      pug: true,
+      mongo: true,
+    },
   },
 ];
 
