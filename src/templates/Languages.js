423e5efd4b0a7247573f50d5b7144af0e7af9736
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-07 21:49:48 +0530

    added title on icons

--- src/templates/Languages.js
+++ src/templates/Languages.js
@@ -6,24 +6,52 @@ import bootstrapIcon from "@iconify-icons/logos/bootstrap";
 import fileTypeReactts from "@iconify-icons/vscode-icons/file-type-reactts";
 import nodejsIcon from "@iconify-icons/logos/nodejs-icon";
 import fileTypePug from "@iconify-icons/vscode-icons/file-type-pug";
+import fileTypeMongo from "@iconify-icons/vscode-icons/file-type-mongo";
 
 const Languages = (props) => {
   const { languages } = props;
   return (
     <div className="Languages d-flex justify-content-around align-content-center flex-wrap ">
-      {languages.html && <Icon icon={fileTypeHtml} width="40" height="40" />}
-      {languages.css && <Icon icon={fileTypeCss} width="40" height="40" />}
+      {languages.html && (
+        <span title="HTML">
+          <Icon icon={fileTypeHtml} width="40" height="40" />
+        </span>
+      )}
+      {languages.css && (
+        <span title="CSS">
+          <Icon icon={fileTypeCss} width="40" height="40" />
+        </span>
+      )}
       {languages.js && (
-        <Icon icon={fileTypeJsOfficial} width="40" height="40" />
+        <span title="Javascript">
+          <Icon icon={fileTypeJsOfficial} width="40" height="40" />
+        </span>
       )}
       {languages.bootstrap && (
-        <Icon icon={bootstrapIcon} width="40" height="40" />
+        <span title="Bootstrap">
+          <Icon icon={bootstrapIcon} width="40" height="40" />
+        </span>
       )}
       {languages.react && (
-        <Icon icon={fileTypeReactts} width="40" height="40" />
+        <span title="React">
+          <Icon icon={fileTypeReactts} width="40" height="40" />
+        </span>
       )}
-      {languages.node && <Icon icon={nodejsIcon} width="40" height="40" />}
-      {languages.pug && <Icon icon={fileTypePug} width="40" height="40" />}
+      {languages.node && (
+        <span title="Node">
+          <Icon icon={nodejsIcon} width="40" height="40" />
+        </span>
+      )}
+      {languages.pug && (
+        <span title="PUG">
+          <Icon icon={fileTypePug} width="40" height="40" />
+        </span>
+      )}
+      {languages.mongo && (
+        <span title="MongoDB">
+          <Icon icon={fileTypeMongo} width="40" height="40" />
+        </span>
+      )}
     </div>
   );
 };
