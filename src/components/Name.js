2f6e3c0c92e784e564989636fa9f24660e98ad22
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-07 14:12:04 +0530

    modified typed properties and changed 'Hello.' color

--- src/components/Name.js
+++ src/components/Name.js
@@ -6,17 +6,17 @@ const Name = () => {
   return (
     <div className="Name">
       <div className="p-5 typed col-md-6" id="name-typed">
-        <FitText compressor={0.9}>
+        <FitText compressor={1}>
           <Typed
             strings={[
-              "<span style='color:###00c0ff;'>Hello.</span>",
+              "<span style='color:#EA0F1E;font-weight:bold'>Hello.</span>",
               "I am Rishabh Pathak <span class='badge badge-pill badge-info'>AKA</span> Clumsyknight.",
               "I am a FullStack developer.",
               "<span style='color:#00ffbf;'>View my projects below.</span>",
             ]}
-            typeSpeed={40}
+            typeSpeed={60}
             smartBackspace={true}
-            backSpeed={60}
+            backSpeed={50}
           />
         </FitText>
       </div>
