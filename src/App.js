6a450fb4708ac6b0155ceb9cc3b3d750a568b2b2
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-07 16:45:22 +0530

    modified component to use router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Name from "./components/Name";
 import "./styles/Scroll.css";
@@ -5,12 +11,19 @@ import "./styles/Scroll.css";
 const App = () => {
   return (
     <div className="App" id="outer-container">
-      <Navbar />
-      <div id="page-wrap">
-        <section className="scroll-child d-flex">
-          <Name />
-        </section>
-      </div>
+      <Router basename="/">
+        <Navbar />
+        <div id="page-wrap">
+          <Switch>
+            <Route exact path="/">
+              <section className="scroll-child d-flex">
+                <Name />
+              </section>
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 };
