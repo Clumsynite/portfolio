60fb3cc30688984e23c17ea497eb395b81631640
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-07 17:21:50 +0530

    modified to use components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import {
   Redirect,
 } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Name from "./components/Name";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import About from "./components/About";
 import "./styles/Scroll.css";
 
 const App = () => {
@@ -13,28 +16,12 @@ const App = () => {
     <div className="App" id="outer-container">
       <Router basename="/">
         <Navbar />
-        <div id="page-wrap">
+        <div id="page-wrap" className="pt-5">
           <Switch>
-            <Route exact path="/">
-              <section className="scroll-child d-flex">
-                <Name />
-              </section>
-            </Route>
-            <Route exact path="/projects">
-              <section className="scroll-child">
-                <h1>Projects</h1>
-              </section>
-            </Route>
-            <Route exact path="/contact">
-              <section className="scroll-child">
-                <h1>Contact</h1>
-              </section>
-            </Route>
-            <Route exact path="/about">
-              <section className="scroll-child">
-                <h1>About</h1>
-              </section>
-            </Route>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/about" component={About} />
             <Redirect to="/" />
           </Switch>
         </div>
