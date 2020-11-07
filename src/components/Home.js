7ad482f99a6638495d49091c1ad44df78536f74f
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-07 18:14:34 +0530

    trying out project cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,24 @@
 import Name from "./Name";
+import ProjectCard from "./ProjectCard";
 
 const Home = () => {
   return (
     <div className="Home">
       <section className="scroll-child">
         <Name />
+      </section>
+      <section className="scroll-child container pt-4">
+        <div class="row row-cols-1 row-cols-md-3">
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </div>
       </section>
     </div>
   );
