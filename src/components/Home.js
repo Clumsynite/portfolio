699003f62db5a3a871ab9d6b31cf579f44240318
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-08 18:18:28 +0530

    imported stylesheet and added section-title class

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import Name from "./Name";
 import ProjectCard from "../templates/ProjectCard";
 import "../templates/projects";
 import projects from "../templates/projects";
+import "../styles/Home.css";
 
 const Home = () => {
   const sixProjects = projects.slice(0, 6);
@@ -11,6 +12,7 @@ const Home = () => {
         <Name />
       </section>
       <section className="scroll-child container pt-4">
+        <h1 className="text-center mb-4 section-title">Recent Projects</h1>
         <div className="row row-cols-1 row-cols-md-3">
           {sixProjects.map((project, index) => (
             <ProjectCard project={project} key={index} />
