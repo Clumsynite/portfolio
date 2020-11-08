0e0ef58d2703984993419da536010b6782bcacfe
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-08 18:35:42 +0530

    added Link to projects page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Name from "./Name";
 import ProjectCard from "../templates/ProjectCard";
 import "../templates/projects";
@@ -11,12 +12,17 @@ const Home = () => {
       <section className="scroll-child">
         <Name />
       </section>
-      <section className="scroll-child container pt-4">
+      <section className="scroll-child container pt-4 mb-5">
         <h1 className="text-center mb-4 section-title">Recent Projects</h1>
         <div className="row row-cols-1 row-cols-md-3">
           {sixProjects.map((project, index) => (
             <ProjectCard project={project} key={index} />
           ))}
+        </div>
+        <div className="curved-div">
+          <Link className="link" to="/projects">
+            View more Projects
+          </Link>
         </div>
       </section>
     </div>
