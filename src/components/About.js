df0865c7ebf89945f53d5014f9be9ab2df43e395
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-10 19:27:39 +0530

    modified components to render section-title

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,11 @@
-import "../styles/Scroll.css";
 import "../styles/Elements.css";
 
 const About = () => {
   return (
     <div className="About">
-      <h1>About</h1>
+      <h1 className="text-center mb-4 pt-3 section-title">About Me</h1>
+
+      
     </div>
   );
 };
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,11 @@
+import "../styles/Elements.css";
+
 const Contact = () => {
   return (
     <div className="Contact">
-      <h1>Contact</h1>{" "}
+      <h1 className="text-center mb-4 pt-3 section-title">Contact Me</h1>
+
+      
     </div>
   );
 };
