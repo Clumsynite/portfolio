c172ab695e35a8f2f9a9d3834024a0a1391bd7ad
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-11 20:04:40 +0530

    added vertical margin

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import "../styles/Elements.css";
 const Contact = () => {
   return (
     <div className="Contact">
-      <h1 className="text-center mb-4 pt-3 section-title">Contact Me</h1>
+      <h1 className="text-center my-5 pt-3 section-title">Contact Me</h1>
       <ContactForm />
     </div>
   );
