ba3ba98320ffe26a3c96cd1cb1f0cca841f55b06
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-11 19:21:41 +0530

    imported ContactForm component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,11 @@
+import ContactForm from "../templates/ContactForm";
 import "../styles/Elements.css";
 
 const Contact = () => {
   return (
     <div className="Contact">
       <h1 className="text-center mb-4 pt-3 section-title">Contact Me</h1>
-
-      
+      <ContactForm />
     </div>
   );
 };
