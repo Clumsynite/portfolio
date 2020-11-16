7e1f7e1d21c2b15094756cfca4b1a9ce44679b6d
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-16 19:53:07 +0530

    render languages as progress bar
    used progress template

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,17 @@
+import Progress from "../templates/Progress";
+import languages from "../data/languages";
 import "../styles/Elements.css";
 
 const About = () => {
   return (
     <div className="About">
       <h1 className="text-center mb-4 pt-3 section-title">About Me</h1>
-
-      
+      <div id="about-body" className="container">
+        {languages.map((language, index) => {
+          const { name, progress } = language;
+          return <Progress language={name} progress={progress} key={index} />;
+        })}
+      </div>
     </div>
   );
 };
