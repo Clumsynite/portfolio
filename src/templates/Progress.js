07c2b053bd359e1a289d950c061fa0a8b89e657b
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-16 19:51:35 +0530

    updated to animate hover event

--- src/templates/Progress.js
+++ src/templates/Progress.js
@@ -1,10 +1,19 @@
 import "../styles/Progress.css";
 
 const Progress = (props) => {
-  const { language, width } = props;
+  const { language, progress } = props;
   return (
     <div className="Progress">
-      <div className="ProgressBar" style={{ width: `${width}%` }}>
+      <div
+        className="ProgressBar"
+        style={{ width: `${100}%` }}
+        onMouseEnter={(e) => {
+          e.target.style.width = `${progress}%`;
+        }}
+        onMouseLeave={(e) => {
+          e.target.style.width = `${100}%`;
+        }}
+      >
         {language}
       </div>
     </div>
