9fe9c7e89fd37087d546a2031edf8ccc5affed51
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-16 19:50:43 +0530

    Create languages.js
    for languages in about me

---
+++ src/data/languages.js
@@ -0,0 +1,13 @@
+const languages = [
+  { name: "HTML", progress: 98 },
+  { name: "CSS", progress: 95 },
+  { name: "Javascript", progress: 85 },
+  { name: "PHP", progress: 75 },
+  { name: "Ruby", progress: 70 },
+  { name: "SQL", progress: 68 },
+  { name: "Java", progress: 65 },
+  { name: "Android", progress: 62 },
+  { name: "Python", progress: 60 },
+];
+
+export default languages;
