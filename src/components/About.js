31aa6058917c3b61430630ff27b2598769efb4a0
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-17 13:51:09 +0530

    added padding top for section title

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,13 @@ import "../styles/Elements.css";
 const About = () => {
   return (
     <div className="About">
-      <h1 className="text-center mb-4 pt-3 section-title">About Me</h1>
+      <h1 className="text-center mt-5 mb-4 pt-3 section-title">About Me</h1>
       <div id="about-body" className="container">
-        {languages.map((language, index) => {
-          const { name, progress } = language;
-          return <Progress language={name} progress={progress} key={index} />;
-        })}
+        <div className="languages-progress col-md-6 mx-auto">
+          {languages.map((language, index) => {
+            return <Progress language={language} key={index} />;
+          })}
+        </div>
       </div>
     </div>
   );
