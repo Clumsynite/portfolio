b6cade0c0946eabba17f6ff478bb9321bd47b3aa
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-17 13:51:23 +0530

    added icon property

--- src/data/languages.js
+++ src/data/languages.js
@@ -1,13 +1,14 @@
 const languages = [
-  { name: "HTML", progress: 98 },
-  { name: "CSS", progress: 95 },
-  { name: "Javascript", progress: 85 },
-  { name: "PHP", progress: 75 },
-  { name: "Ruby", progress: 70 },
-  { name: "SQL", progress: 68 },
-  { name: "Java", progress: 65 },
-  { name: "Android", progress: 62 },
-  { name: "Python", progress: 60 },
+  { name: "HTML", progress: 98, icon: { html: true } },
+  { name: "CSS", progress: 95, icon: { css: true } },
+  { name: "Javascript", progress: 85, icon: { javascript: true } },
+  { name: "PHP", progress: 75, icon: { php: true } },
+  { name: "Ruby", progress: 70, icon: { ruby: true } },
+  { name: "NoSQL", progress: 70, icon: { nosql: true } },
+  { name: "SQL", progress: 68, icon: { sql: true } },
+  { name: "Java", progress: 65, icon: { java: true } },
+  { name: "Android", progress: 62, icon: { android: true } },
+  { name: "Python", progress: 60, icon: { python: true } },
 ];
 
 export default languages;
