4324ad859392ca8aba8fcdda54bbef70fc33613a
Clumsy Knight <clusmsyknight@gmail.com>
2020-11-21 17:02:17 +0530

    altered title, removed tooltip comment

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,6 @@ import {
 } from "react-accessible-accordion";
 import "react-accessible-accordion/dist/fancy-example.css";
 import { Link } from "react-router-dom";
-import ReactTooltip from "react-tooltip";
 import Progress from "../templates/Progress";
 import SkillIcons from "../templates/Languages-Project";
 import languages from "../data/languages";
@@ -41,20 +40,9 @@ const About = () => {
           >
             Rishabh Pathak
           </span>
-          {/* <ReactTooltip id="rishabh" aria-haspopup="true">
-            <div className="card bg-light text-dark">
-              <div className="card-body">
-                <h5 className="card-title"> Rishabh Jitendra Pathak</h5>
-                <p className="card-text">Age: 20</p>
-                <p className="card-text">Email: rishabhp82@gmail.com</p>
-              </div>
-            </div>
-          </ReactTooltip> */}
-          , a Fullstack web developer. I have been using the pseudonym{" "}
-          <span className="text-info font-italic">Clumsyknight</span> in my
-          projects. You can find some of those projects on the{" "}
+          , a Fullstack web developer. You can find some of my projects on{" "}
           <Link to="/projects" className="link">
-            Projects
+            this
           </Link>{" "}
           page or on my{" "}
           <a
@@ -65,7 +53,8 @@ const About = () => {
           >
             <LanguageIcons icon={{ github: true }} /> Profile
           </a>
-          .
+          . <span className="text-danger font-italic">Clumsyknight</span>, is a
+          pseudonym that I often use in those projects.
         </div>
         <div className="col-md-6 mx-auto">
           <Accordion allowZeroExpanded>
